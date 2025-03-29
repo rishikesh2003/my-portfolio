291456f0aa9c3004869447921ad37334e61f4421
Henry Valdez <henryvaldez@gmail.com>
2025-03-29 11:27:51 -0500

    actualizando mas iconos

--- src/components/ZerpaSection.tsx
+++ src/components/ZerpaSection.tsx
@@ -20,7 +20,7 @@ export default function ExperienceSection() {
               transition={{ duration: 0.5, delay: 0.2 }}
               viewport={{ once: true }}
             >
-              💼
+              🎓
             </motion.span>{" "}
             Eladio Zerpa Ortega
           </h2>
