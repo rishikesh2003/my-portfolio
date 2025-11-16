776139dd8380d2ff62ebe7ac76fc59117ff9fe1c
Kyle Hancock <kyle.hancock@jasprhealth.com>
2025-11-16 16:09:00 -0500

    Correct path issue and setup for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kahancock.github.io',
-  base: '/portfolio',
+  site: 'https://kylehancock.com',
   output: 'static',
   vite: {
       plugins: [tailwindcss()],
