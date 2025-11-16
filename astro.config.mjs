d7cccf247ad94df019063774f0462cbea129c554
Kyle Hancock <kyle.hancock@jasprhealth.com>
2025-11-16 16:29:24 -0500

    update URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kylehancock.com',
+  site: 'https://www.kylehancock.com',
   output: 'static',
   vite: {
       plugins: [tailwindcss()],
